010721fc8cf49e771d2883989cca62dac11b274c
aymanefer <fer.aymane@gmail.com>
2022-08-26 19:44:53 +0100

    Added background on hover

--- javascript/main.js
+++ javascript/main.js
@@ -11,4 +11,37 @@ $(document).ready(function () {
       // $(".nav-link").removeClass("nav-link-scrolled");
     }
   });
+
+  function showbg1(name) {
+    let image_path = "url(assets/backgrounds/" + name + ".jpg)";
+    $("#anime-background1").css("opacity", "1");
+    $("#anime-background1").css("background-image", image_path);
+  }
+
+  function hidebg1() {
+    $("#anime-background1").css("opacity", "0");
+  }
+
+  function showbg2(name) {
+    let image_path = "url(assets/backgrounds/" + name + ".jpg)";
+    $("#anime-background2").css("opacity", "1");
+    $("#anime-background2").css("background-image", image_path);
+  }
+
+  function hidebg2() {
+    $("#anime-background2").css("opacity", "0");
+  }
+
+  $("#popular .item-image").hover(function () {
+    let id = $(this).attr("id");
+    console.log("the image is", id);
+    showbg1(id);
+  }, hidebg1);
+
+  $("#upcoming .item-image").hover(function () {
+    let id = $(this).attr("id");
+    console.log("the image is", id);
+    showbg2(id);
+  }, hidebg2);
+
 });
